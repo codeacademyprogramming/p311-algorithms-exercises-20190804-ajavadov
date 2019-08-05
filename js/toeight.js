382795473eceb843f2a5fb9df135763443c25f41
ajavadov <ajavadov2020@ada.edu.az>
2019-08-05 18:59:10 +0400

    updated

--- js/toeight.js
+++ js/toeight.js
@@ -1,25 +1,38 @@
 alert("a, b ve c ededlerini daxil edin");
-var llist=[];
-var newllist=[];
+var llist = [];
+var newllist = [];
+var list3 = [];
 llist.push(parseInt(prompt("a?")));
 llist.push(parseInt(prompt("b?")));
 llist.push(parseInt(prompt("c?")));
-console.log(llist)
-if(llist[0]%8===0){
-    // alert(list[0]+" hem 3-e hem de 5-e qaliqsiz bolunur")
+console.log(llist);
+let flag = 0;
+if (llist[0] % 2 === 0 && llist[0] % 4 === 0) {
+  if (llist[0] % 8 === 0) {
     newllist.push(llist[0]);
+  } else {
+    list3.push(llist[0]);
+  }
 }
-if(llist[1]%8===0){
-    newllist.push(llist[1]);
-
+if (llist[1] % 2 === 0 && llist[1] % 4 === 0) {
+  if (llist[1] % 8 === 0) {
+    newllist.push(llist[0]);
+  } else {
+    list3.push(llist[1]);
+  }
 }
-if(llist[2]%8===0){
+if (llist[2] % 2 === 0 && llist[2] % 4 === 0) {
+  if (llist[2] % 8 === 0) {
     newllist.push(llist[2]);
-
+  } else {
+    list3.push(llist[2]);
+  }
 }
-if(newllist.length>0)
-alert(newllist.join(' , ')+" hem 2-e hem de 4-e qaliqsiz bolunur.")
-else{
-    alert('hecbiri')
+if (newllist.length > 0)
+  alert(
+    newllist.join(" , ") + ", hem 2-ye hem 4-e hem de 8-e qaliqsiz bolunur."
+  );
+else "hec 2 ve 4e de bolunmur";
+if (list3.length > 0) {
+  alert(list3.join(" , ") + " ise, 2-ye ve 4-e bolunur amma 8-e yox.");
 }
-
